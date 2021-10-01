c35644b8e8581a659eeb90b5b86ae4b73eba819a
LBruner <leandrobruner15@gmail.com>
2021-10-01 07:59:12 -0300

    vide 4

---
+++ Projeto Integrador IV/Assets/Scripts/UI/Quests/QuestListUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Quests;
+
+namespace RPG.UI.Quests
+{
+    public class QuestListUI : MonoBehaviour
+    {
+        [SerializeField] Quest[] tempQuests;
+        [SerializeField] QuestItemUI questPrefab;
+
+        private void Start()
+        {
+            transform.DetachChildren();
+            foreach (Quest quest in tempQuests)
+            {
+                QuestItemUI uiInstance = Instantiate<QuestItemUI>(questPrefab, transform);
+                uiInstance.Setup(quest);
+            }
+        }
+    }
+}
