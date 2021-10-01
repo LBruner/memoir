22dc8d01754e9157d320107f25dd8110ed46857a
LBruner <leandrobruner15@gmail.com>
2021-10-01 07:07:00 -0300

    video 3

---
+++ Projeto Integrador IV/Assets/Scripts/UI/Quests/QuestTooltipSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.UI;
+using UnityEngine;
+
+public class QuestTooltipSpawner : TooltipSpawner
+{
+    public override bool CanCreateTooltip()
+    {
+        return true;
+    }
+
+    public override void UpdateTooltip(GameObject tooltip)
+    {
+
+    }
+}
